73b9b0f6f499a01467b2cb0917b33f6b6cb5ec26
Ilux-i <iluxeo6@gmail.com>
2025-09-11 21:20:26 +0300

    .
    .

--- src/main/java/manager/InMemoryHistoryManager.java
+++ src/main/java/manager/InMemoryHistoryManager.java
@@ -65,38 +65,3 @@ public class InMemoryHistoryManager implements HistoryManager {
         }
     }
 }
-
-class Node {
-    private Node next;
-    private Task core;
-    private Node prev;
-
-    Node(Node prev, Task core) {
-        this.core = core;
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Task getCore() {
-        return core;
-    }
-
-    public void setCore(Task core) {
-        this.core = core;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-}
