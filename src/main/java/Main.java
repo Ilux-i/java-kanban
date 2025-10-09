e883dafee42e68a6bb4f44ad352a9bb70db8910b
Ilux-i <iluxeo6@gmail.com>
2025-10-09 23:12:58 +0300

    refactor
    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,6 @@
 package main.java;
 
-import main.java.manager.FileBackedTaskManager;
-
 public class Main {
     public static void main(String[] args) {
-        FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile("data_test.csv");
-//        manager.addTask(new Task("task1", "description4", Duration.ofMinutes(90L), LocalDateTime.now()));
-//        manager.addEpic(new Epic("epic", "description4", Duration.ofMinutes(30), LocalDateTime.now()));
-//        manager.addSubTask(new SubTask("subtask", "description4", 0, Duration.ofMinutes(30), LocalDateTime.now()));
-//        System.out.println(manager.getEpicById(0).getStartTime());
-//        System.out.println(manager.getEpicById(0).getDuration().toMinutes());
-//        System.out.println(manager.getEpicById(0).getEndTime());
-//        System.out.println(manager.getPrioritizedTasks());
-
-
     }
 }
